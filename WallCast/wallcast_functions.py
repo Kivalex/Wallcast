c728095ff2d3b4b8ad7bebf099c34cc9d990ad57
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2023-01-31 10:06:20 +0300

    Update wallcast_functions.py

--- WallCast/wallcast_functions.py
+++ WallCast/wallcast_functions.py
@@ -1,9 +1,1 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 
-
-def function_wallcast(self):
-    self.pushButton_tape.clicked.connect(self.wallcast_tape_but_click)
-
-
-def wallcast_tape_but_click(self):
-    self.frame_2.setVisible(True)

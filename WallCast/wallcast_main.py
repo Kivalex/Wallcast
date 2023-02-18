403f712c66f0030a752564ad4054c02cbb3fa0ee
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2023-02-18 21:19:53 +0300

    Update wallcast_main.py

--- WallCast/wallcast_main.py
+++ WallCast/wallcast_main.py
@@ -81,27 +81,27 @@ class Ui_MainWindow(QMainWindow, object):
         self.pushButton_profil.setIconSize(QtCore.QSize(77, 54))
         self.pushButton_profil.setAutoRepeat(False)
         self.pushButton_profil.setObjectName("pushButton_profil")
-        self.pushButton = QtWidgets.QPushButton(self.box_top_selection)
-        self.pushButton.setGeometry(QtCore.QRect(1160, 16, 41, 41))
-        self.pushButton.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+        self.notifications_pushbutton = QtWidgets.QPushButton(self.box_top_selection)
+        self.notifications_pushbutton.setGeometry(QtCore.QRect(1160, 16, 41, 41))
+        self.notifications_pushbutton.setStyleSheet("background-color: rgb(255, 255, 255);\n"
 "border-style: outset;\n"
 "border-width: 2px;\n"
 "border-radius: 20px;\n"
 "border-color: beige;\n"
 "font: bold 16px;\n"
 "padding: 6px;")
-        self.pushButton.setText("")
+        self.notifications_pushbutton.setText("")
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap("C:\\Users\\PC\\PycharmProjects\\pythonProject\\WallCast\\kolokol_wallcast.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon1)
-        self.pushButton.setIconSize(QtCore.QSize(28, 28))
-        self.pushButton.setObjectName("pushButton")
+        self.notifications_pushbutton.setIcon(icon1)
+        self.notifications_pushbutton.setIconSize(QtCore.QSize(28, 28))
+        self.notifications_pushbutton.setObjectName("pushButton")
         self.label_logo.raise_()
         self.pushButton_subscriptions.raise_()
         self.pushButton_messages.raise_()
         self.pushButton_profil.raise_()
         self.pushButton_tape.raise_()
-        self.pushButton.raise_()
+        self.notifications_pushbutton.raise_()
         self.fon_subscriptions = QtWidgets.QFrame(self.centralwidget)
         self.fon_subscriptions.setVisible(False)
         self.fon_subscriptions.setGeometry(QtCore.QRect(160, 61, 960, 661))
@@ -252,6 +252,78 @@ class Ui_MainWindow(QMainWindow, object):
         self.pushbutton_hide_window2.setObjectName("pushButton_3")
         self.pushbutton_hide_window1.setVisible(False)
         self.pushbutton_hide_window2.setVisible(False)
+        self.box_notifications = QtWidgets.QFrame(self.centralwidget)
+        self.box_notifications.setGeometry(QtCore.QRect(990, 70, 291, 271))
+        self.box_notifications.setMouseTracking(False)
+        self.box_notifications.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+                                             "border-bottom-left-radius: 20px;")
+        self.box_notifications.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.box_notifications.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.box_notifications.setObjectName("box_notifications")
+        self.box_notifications.setVisible(False)
+        self.box_notifications_ava = QtWidgets.QPushButton(self.box_notifications)
+        self.box_notifications_ava.setGeometry(QtCore.QRect(25, 17, 51, 51))
+        self.box_notifications_ava.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-color: beige;\n"
+"font: bold 16px;\n"
+"padding: 6px;\n"
+"border-bottom-left-radius: 0px;")
+        self.box_notifications_ava.setText("")
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("C:\\Users\\PC\\PycharmProjects\\pythonProject\\WallCast\\ava_kivalex.jpg"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.box_notifications_ava.setIcon(icon3)
+        self.box_notifications_ava.setIconSize(QtCore.QSize(51, 51))
+        self.box_notifications_ava.setObjectName("box_notifications_ava")
+        self.box_notifications_name1 = QtWidgets.QPushButton(self.box_notifications)
+        self.box_notifications_name1.setGeometry(QtCore.QRect(10, 70, 81, 23))
+        self.box_notifications_name1.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+"color: rgb(255, 255, 255);\n"
+"border-bottom-left-radius: 0px;\n"
+"font: bold 0px;")
+        self.box_notifications_name1.setObjectName("pushButton_4")
+        self.textEdit = QtWidgets.QTextEdit(self.box_notifications)
+        self.textEdit.setGeometry(QtCore.QRect(90, 2, 181, 81))
+        self.textEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-bottom-left-radius: 0px;")
+        self.textEdit.setObjectName("textEdit")
+        self.pushbutton_box_notifications_gotopodcast = QtWidgets.QPushButton(self.box_notifications)
+        self.pushbutton_box_notifications_gotopodcast.setGeometry(QtCore.QRect(90, 90, 181, 23))
+        self.pushbutton_box_notifications_gotopodcast.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-bottom-left-radius: 0px;")
+        self.pushbutton_box_notifications_gotopodcast.setObjectName("pushButton_5")
+        self.box_notifications_ava_2 = QtWidgets.QPushButton(self.box_notifications)
+        self.box_notifications_ava_2.setGeometry(QtCore.QRect(25, 137, 51, 51))
+        self.box_notifications_ava_2.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-color: beige;\n"
+"font: bold 16px;\n"
+"padding: 6px;\n"
+"border-bottom-left-radius: 0px;")
+        self.box_notifications_ava_2.setText("")
+        self.box_notifications_ava_2.setIcon(icon3)
+        self.box_notifications_ava_2.setIconSize(QtCore.QSize(51, 51))
+        self.box_notifications_ava_2.setObjectName("box_notifications_ava_2")
+        self.pushbutton_box_notifications_gotopodcast2 = QtWidgets.QPushButton(self.box_notifications)
+        self.pushbutton_box_notifications_gotopodcast2.setGeometry(QtCore.QRect(90, 210, 181, 23))
+        self.pushbutton_box_notifications_gotopodcast2.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-bottom-left-radius: 0px;")
+        self.pushbutton_box_notifications_gotopodcast2.setObjectName("pushButton_6")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.box_notifications)
+        self.textEdit_2.setGeometry(QtCore.QRect(90, 122, 181, 81))
+        self.textEdit_2.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-bottom-left-radius: 0px;")
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.box_notifications_name2 = QtWidgets.QPushButton(self.box_notifications)
+        self.box_notifications_name2.setGeometry(QtCore.QRect(10, 190, 81, 23))
+        self.box_notifications_name2.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+"color: rgb(255, 255, 255);\n"
+"border-bottom-left-radius: 0px;\n"
+"font: bold 0px;")
+        self.box_notifications_name2.setObjectName("pushButton_8")
         self.frame = QtWidgets.QFrame(self.centralwidget)
         self.frame.setGeometry(QtCore.QRect(160, 60, 960, 661))
         self.frame.setStyleSheet("background-color: rgb(255, 255, 255);")
@@ -265,6 +337,7 @@ class Ui_MainWindow(QMainWindow, object):
         self.box_tape_slection.raise_()
         self.pushbutton_hide_window1.raise_()
         self.pushbutton_hide_window2.raise_()
+        self.box_notifications.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -289,6 +362,26 @@ class Ui_MainWindow(QMainWindow, object):
         self.settings_button_profil.setText(_translate("MainWindow", "Настройки"))
         self.log_off_button_profil.setText(_translate("MainWindow", "Выйти"))
         self.change_account_button_profil.setText(_translate("MainWindow", "Сменить аккаунт"))
+        self.box_notifications_name1.setText(_translate("MainWindow", "Kivalex"))
+        self.textEdit.setHtml(_translate("MainWindow",
+                                         "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                         "p, li { white-space: pre-wrap; }\n"
+                                         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Название:</p>\n"
+                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Тема:</p>\n"
+                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Описание: шцуипшгзгуиывтолитвылфмлдьцфшурпуццуппцупуцпуупцупцупц</p></body></html>"))
+        self.pushbutton_box_notifications_gotopodcast.setText(_translate("MainWindow", "Перейти на подкаст"))
+        self.pushbutton_box_notifications_gotopodcast2.setText(_translate("MainWindow", "Перейти на подкаст"))
+        self.textEdit_2.setHtml(_translate("MainWindow",
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                           "p, li { white-space: pre-wrap; }\n"
+                                           "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Название:</p>\n"
+                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Тема:</p>\n"
+                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Описание: шцуипшгзгуиывтолитвылфмлдьцфшурпуццуппцупуцпуупцупцупц</p></body></html>"))
+        self.box_notifications_name2.setText(_translate("MainWindow", "kr7a-J"))
 
 
     '''Все функции кнопок'''
@@ -297,8 +390,9 @@ class Ui_MainWindow(QMainWindow, object):
         self.pushButton_profil.clicked.connect(self.wallcast_profil_top_button_click)
         self.pushButton_subscriptions.clicked.connect(self.wallcast_subscriptions_button_click)
         self.pushButton_messages.clicked.connect(self.wallcast_messages_button_click)
-        self.pushbutton_hide_window1.clicked.connect(self.hide_window_after_clicked)
-        self.pushbutton_hide_window2.clicked.connect(self.hide_window_after_clicked)
+        self.pushbutton_hide_window1.clicked.connect(self.wallcast_hide_window_after_click)
+        self.pushbutton_hide_window2.clicked.connect(self.wallcast_hide_window_after_click)
+        self.notifications_pushbutton.clicked.connect(self.wallcast_notification_button_click)
 
 
     def wallcast_tape_button_click(self):    #При нажатии на кнопку 'Лента' происходит
@@ -306,6 +400,8 @@ class Ui_MainWindow(QMainWindow, object):
         self.pushButton_tape_2.clicked.connect(self.wallcast_tape_2_button_click)
     def wallcast_profil_top_button_click(self):    #При нажатии на кнопку 'Профиль' происходит
         self.box_settings.setVisible(True)
+        self.pushbutton_hide_window1.setGeometry(QtCore.QRect(0, 290, 1281, 431))
+        self.pushbutton_hide_window2.setGeometry(QtCore.QRect(0, 70, 1131, 291))
         self.pushbutton_hide_window1.setVisible(True)
         self.pushbutton_hide_window2.setVisible(True)
     def wallcast_tape_2_button_click(self):    #При нажатии на кнопку 'Глобальная лента' происходит
@@ -315,10 +411,17 @@ class Ui_MainWindow(QMainWindow, object):
         self.fon_subscriptions.setVisible(False)
     def wallcast_messages_button_click(self):    #При нажатии на кнопку 'Сообщения' происходит
         self.fon_subscriptions.setVisible(False)
-    def hide_window_after_clicked(self):    #Скрывает окно профиля при нажатии в любое место на экране
+    def wallcast_hide_window_after_click(self):    #Скрывает окна (профиль и уведомления) при нажатии в любое место на экране
         self.box_settings.setVisible(False)
+        self.box_notifications.setVisible(False)
         self.pushbutton_hide_window1.setVisible(False)
         self.pushbutton_hide_window2.setVisible(False)
+    def wallcast_notification_button_click(self):
+        self.box_notifications.setVisible(True)
+        self.pushbutton_hide_window1.setGeometry(QtCore.QRect(0, 340, 1281, 381))
+        self.pushbutton_hide_window2.setGeometry(QtCore.QRect(0, 0, 991, 291))
+        self.pushbutton_hide_window1.setVisible(True)
+        self.pushbutton_hide_window2.setVisible(True)
 
 
 '''Запуск программы'''

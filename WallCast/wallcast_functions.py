1b36f5913ec7587ad9a8256e22ba2bb6810cfd2c
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2023-02-28 23:19:34 +0300

    Update wallcast_functions.py

--- WallCast/wallcast_functions.py
+++ WallCast/wallcast_functions.py
@@ -1,12 +1,25 @@
 import getpass
 import os
-path = "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\"
+import re
+
+'''Путь к файлам'''
 disk = os.getenv("SystemDrive")
 user_login = getpass.getuser()
-logo_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path +"logo_wallcast.png")
-ava_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "ava_wallcast.png")
-kolokol_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "kolokol_wallcast.png")
-ava_kivalex = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "ava_kivalex.jpg")
-open_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "open_eye.png")
-closed_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "closed_eye.png")
-print(closed_eye)
+logo_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\logo_wallcast.png")
+ava_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\ava_wallcast.png")
+kolokol_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\kolokol_wallcast.png")
+ava_kivalex = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\ava_kivalex.jpg")
+open_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\open_eye.png")
+closed_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\closed_eye.png")
+
+'''Проверка пароля на наличие необходимых знаков'''
+def validate_password(text):
+    if not re.search(r'\d', text):  # Проверка наличия цифры
+        return False
+    if not re.search(r'[!@#^*]', text):  # Проверка наличия специального символа
+        return False
+    if not re.search(r'[a-zA-Z]', text):  # Проверка наличия латинской буквы
+        return False
+    if len(text) < 6 or len(text) > 18:
+        return False
+    return True

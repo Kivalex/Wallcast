1f76090ca0ce5187fbd93cab86fab81577d1d657
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2023-02-28 13:52:14 +0300

    Update wallcast_functions.py

--- WallCast/wallcast_functions.py
+++ WallCast/wallcast_functions.py
@@ -1,10 +1,12 @@
 import getpass
 import os
+path = "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\"
 disk = os.getenv("SystemDrive")
 user_login = getpass.getuser()
-logo_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\logo_wallcast.png")
-ava_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\ava_wallcast.png")
-kolokol_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\kolokol_wallcast.png")
-ava_kivalex = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\ava_kivalex.jpg")
-open_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\open_eye.png")
-closed_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\closed_eye.png")
+logo_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path +"logo_wallcast.png")
+ava_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "ava_wallcast.png")
+kolokol_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "kolokol_wallcast.png")
+ava_kivalex = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "ava_kivalex.jpg")
+open_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "open_eye.png")
+closed_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + path + "closed_eye.png")
+print(closed_eye)

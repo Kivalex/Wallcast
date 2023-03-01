b5ab464d469f9d1831506e17e1559f95f53f808a
Kivalex <122360027+Kivalex@users.noreply.github.com>
2023-03-02 01:49:59 +0300

    Add files via upload

--- WallCast/wallcast_functions.py
+++ WallCast/wallcast_functions.py
@@ -1,8 +1,10 @@
 import getpass
 import os
-import re
 
-'''Путь к файлам'''
+import pyaudio
+import wave
+import keyboard
+
 disk = os.getenv("SystemDrive")
 user_login = getpass.getuser()
 logo_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\logo_wallcast.png")
@@ -12,19 +14,48 @@ ava_kivalex = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\
 open_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\open_eye.png")
 closed_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\closed_eye.png")
 
-'''Проверка пароля на наличие необходимых знаков'''
-def validate_password(text):
-    if not re.search(r'\d', text):  # Проверка наличия цифры
-        return False
-    if not re.search(r'[!@#^*]', text):  # Проверка наличия специального символа
-        return False
-    if not re.search(r'[a-zA-Z]', text):  # Проверка наличия латинской буквы
-        return False
-    if not re.search(r'[A-Z]', text):  # Проверка наличия латинской буквы
-        return False
-    if len(text) < 6 or len(text) > 18:
-        return False
-    return True
 
-def validate_name(text):
-    pass
+
+def record_audio(filename):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    RECORD_SECONDS = 600
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("Нажмите клавишу для начала записи")
+    keyboard.wait('enter')
+
+    frames = []
+
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+        if keyboard.is_pressed('q'):
+            print("Recording stopped.")
+            break
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    print("Saving recording...")
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    print("Recording saved as", filename)
+record_audio("my_audio.wav")

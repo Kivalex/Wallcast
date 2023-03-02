d839f209cf93390bbb9b9cd4ee5d5b6b329108c1
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2023-03-02 14:51:12 +0300

    Update wallcast_functions.py

--- WallCast/wallcast_functions.py
+++ WallCast/wallcast_functions.py
@@ -1,10 +1,12 @@
 import getpass
 import os
+import re
 
 import pyaudio
 import wave
 import keyboard
 
+'''Получение пути к файлам'''
 disk = os.getenv("SystemDrive")
 user_login = getpass.getuser()
 logo_wallcast = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\logo_wallcast.png")
@@ -14,8 +16,25 @@ ava_kivalex = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\
 open_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\open_eye.png")
 closed_eye = str(str(disk) + "\\\\" + "Users" + "\\\\" + str(user_login) + "\\\\PycharmProjects\\\\pythonProject\\\\WallCast\\\\closed_eye.png")
 
+'''Проверка пароля на наличие необходимых знаков'''
+def validate_password(text):
+    if not re.search(r'\d', text):  # Проверка наличия цифры
+        return False
+    if not re.search(r'[!@#^*]', text):  # Проверка наличия специального символа
+        return False
+    if not re.search(r'[a-zA-Z]', text):  # Проверка наличия латинской буквы
+        return False
+    if not re.search(r'[A-Z]', text):  # Проверка наличия латинской буквы
+        return False
+    if len(text) < 6 or len(text) > 18:
+        return False
+    return True
 
+'''Проверка имени на доступность'''
+def validate_name(text):
+    pass
 
+'''Запись аудио'''
 def record_audio(filename):
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
